968d868a950c532384d14c855f53f307040281fd
Nicolas-Barboza <nicobarboza85@email.com>
2025-06-12 03:32:35 -0300

    feat(setup): Implementar estructura de enrutamiento y diseño en el componente de la aplicación; agregar estilos Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'; 
+import './index.css'; 
+
+
+const Home = () => <div><h1>¡Bienvenido a Casa!</h1><p style={{textAlign: 'center', marginTop: '20px'}}>Esta es la página de Inicio.</p></div>;
+const ProductForm = () => <div><h1>Formulario de Producto</h1><p style={{textAlign: 'center', marginTop: '20px'}}>Esta página permitirá añadir/editar productos.</p></div>;
+const FavoritesPage = () => <div><h1>Favoritos</h1><p style={{textAlign: 'center', marginTop: '20px'}}>Esta página mostrará productos favoritos.</p></div>;
+const ProductDetail = () => <div><h1>Detalle del Producto</h1><p style={{textAlign: 'center', marginTop: '20px'}}>Esta página mostrará los detalles del producto.</p></div>;
+const Acercade = () => <div><h1>Acerca de Nosotros</h1><p style={{textAlign: 'center', marginTop: '20px'}}>Información sobre los desarrolladores.</p></div>;
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Router>
+        <div className="app-container">
+          <main className="content-wrap" style={{ paddingTop: "10px" }}>
+            <div className="main-content-area">
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/products/:id" element={<ProductDetail />} />
+                <Route path="/products/new" element={<ProductForm />} />
+                <Route path="/products/:id/edit" element={<ProductForm />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+                <Route path="/acerca" element={<Acercade />} />
+              </Routes>
+            </div>
+          </main>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
