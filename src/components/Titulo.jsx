04dc5f35d2ba59e57d9ad8590ffbf84209496e92
mikaela coca <mika13coca@gmail.com>
2025-06-13 18:38:15 -0300

    creacion del productform junto a su css y la creacion del componente titulo

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,15 @@
+function Titulo({ texto }) {
+    const style = {
+        fontFamily: "Lato, sans-serif",
+        fontSize: "35px",         
+        fontWeight: "600",        
+        color: "#2c3e50",         
+        margin: "30px 0 25px 0",   
+        textAlign: "center",
+        letterSpacing: "0.5px",    
+    };
+    return (
+        <h2 style={style}>{texto}</h2>
+    );
+}
+export default Titulo;
