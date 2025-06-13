3b0447e6f92f1c9c85887e873a61b4d684ae9ab9
Facundo254 <facusancor.com.ar007@gmail.com>
2025-06-12 21:15:16 -0300

    Cambios del componente Notification

---
+++ src/components/Notification.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import '../styles/Notification.css'; 
+
+const Notification = ({ mensaje, tipo, onClose }) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+    onClose();
+    }, 2500);
+    return () => clearTimeout(timer);
+    }, [mensaje, onClose]); 
+
+    if (!mensaje) {
+        return null;
+    }
+
+  // Determina la clase según el tipo de notificación para estilos
+const notificationClass = `notification ${tipo === 'success' ? 'success' : 'error'}`;
+
+return (
+    <div className={notificationClass}>
+    <p>{mensaje}</p>
+    <button onClick={onClose} className="close-btn">
+        &times; 
+    </button>
+    </div>
+);
+};
+export default Notification;
