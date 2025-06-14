c20a0604db7387661136472b4b4329c79f5f8c90
Nicolas-Barboza <nicobarboza85@email.com>
2025-06-14 02:09:49 -0300

    feat(navbar): Implementar nuevo color, marcación de activo y icono de título bi-color.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,27 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaHome, FaTags, FaInfoCircle, FaHeart } from 'react-icons/fa';
+import { FaHome, FaInfoCircle, FaHeart } from 'react-icons/fa'; 
+import { BsFillBoxSeamFill} from 'react-icons/bs';
 import '../styles/CustomNavBar.css';
 
 function NavBar() {
     return (
         <Navbar fixed="top" expand="lg" variant="dark" className="custom-navbar">
             <Container fluid>
-                <Navbar.Brand as={Link} to="/home" className="custom-navbar-brand"> Gestión de Productos</Navbar.Brand>
+                <Navbar.Brand as={NavLink} to="/home" className="custom-navbar-brand">
+                    <BsFillBoxSeamFill className="me-2 icono-titulo" />
+                    Sistema de Gestión de Productos
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto align-items-center">
-                        <Nav.Link as={Link} to="/home" className="custom-nav-link">
+                        <Nav.Link as={NavLink} to="/home" className="custom-nav-link">
                             <FaHome className="me-1" /> Inicio
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/products/new" className="custom-nav-link">
-                            <FaTags className="me-1" /> Nuevo Producto
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/favorites" className="custom-nav-link">
+                        <Nav.Link as={NavLink} to="/favorites" className="custom-nav-link">
                             <FaHeart className="me-1" /> Favoritos
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/acerca" className="custom-nav-link">
+                        <Nav.Link as={NavLink} to="/acerca" className="custom-nav-link">
                             <FaInfoCircle className="me-1" /> Acerca de
                         </Nav.Link>
                     </Nav>
